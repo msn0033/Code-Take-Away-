256b694ac7f417a5d3532bf598be9268f7c04d46
murad <msn0033@gmail.com>
2022-07-27 13:43:52 +0300

    003 ImmutableVsMutable

---
+++ 003 ImmutableVsMutable/CodeToGo.MutableClass/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CodeToGo.MutableClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var date1 = new Date(2022, 02, 28);
+
+            date1.Day = 40; // Error compile time
+
+            Console.WriteLine($"{date1}");
+
+             date1.AddDays(10); // Mutable
+
+            Console.WriteLine($"{date1}"); 
+
+        }
+    }
+}
